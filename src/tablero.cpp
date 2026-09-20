ce6dc24182d71c1bbea1da7aa9142031967f6c84
dortiz02 <dortiz.romero@udea.edu.co>
2026-09-20 16:34:52 -0500

    Update tablero.cpp

--- src/tablero.cpp
+++ src/tablero.cpp
@@ -8,8 +8,6 @@ unsigned char* crearTablero(int filas, int columnas, int &bytesReservados)
 
     unsigned char* tablero = new unsigned char[bytesReservados];
 
-    // Por ahora se deja todo en cero; el llenado con fichas aleatorias
-    // se agrega en un commit posterior.
     for (int i = 0; i < bytesReservados; i++) {
         tablero[i] = 0;
     }
@@ -20,4 +18,31 @@ unsigned char* crearTablero(int filas, int columnas, int &bytesReservados)
 void liberarTablero(unsigned char* tablero)
 {
     delete[] tablero;
+}
+
+unsigned char obtenerFicha(unsigned char* tablero, int fila, int columna, int columnas)
+{
+    int indice = fila * columnas + columna;
+    int bitInicial = indice * BITS_POR_FICHA;
+
+    int byteIndice = bitInicial / 8;
+    int bitOffset = bitInicial % 8;
+
+    unsigned char mascara = 0b111 << bitOffset;
+    unsigned char valor = (tablero[byteIndice] & mascara) >> bitOffset;
+
+    return valor;
+}
+
+void colocarFicha(unsigned char* tablero, int fila, int columna, int columnas, unsigned char valorFicha)
+{
+    int indice = fila * columnas + columna;
+    int bitInicial = indice * BITS_POR_FICHA;
+
+    int byteIndice = bitInicial / 8;
+    int bitOffset = bitInicial % 8;
+
+    unsigned char mascaraLimpiar = ~(0b111 << bitOffset);
+    tablero[byteIndice] = tablero[byteIndice] & mascaraLimpiar;
+    tablero[byteIndice] = tablero[byteIndice] | (valorFicha << bitOffset);
 }
