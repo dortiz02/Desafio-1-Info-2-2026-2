6f2e2b75ee9acc60251d5d1afa7cf416eae8cba9
dortiz02 <dortiz.romero@udea.edu.co>
2026-09-20 17:17:43 -0500

    Update tablero.h
    se agrega la declaracion de llenarTableroAleatorio, que va a usar el generador del modulo aleatorio para completar el tablero de una sola vez.

--- include/tablero.h
+++ include/tablero.h
@@ -1,8 +1,8 @@
 #ifndef TABLERO_H
 #define TABLERO_H
 
-// Crea un tablero de filas x columnas, reservando la cantidad mínima
-// de bytes necesaria para almacenar 3 bits por posición.
+// Crea un tablero de 'filas' x 'columnas', reservando la cantidad minima
+// de bytes necesaria para almacenar 3 bits por posicion.
 unsigned char* crearTablero(int filas, int columnas, int &bytesReservados);
 
 // Libera la memoria asociada a un tablero.
@@ -11,7 +11,10 @@ void liberarTablero(unsigned char* tablero);
 // Obtiene el codigo de 3 bits almacenado en (fila, columna).
 unsigned char obtenerFicha(unsigned char* tablero, int fila, int columna, int columnas);
 
-// Escribe el codigo de 3 bits valorFicha en (fila, columna).
+// Escribe el codigo de 3 bits 'valorFicha' en (fila, columna).
 void colocarFicha(unsigned char* tablero, int fila, int columna, int columnas, unsigned char valorFicha);
+
+// Llena todas las posiciones del tablero con fichas aleatorias.
+void llenarTableroAleatorio(unsigned char* tablero, int filas, int columnas);
 
 #endif // TABLERO_H
