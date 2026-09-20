b03bb9e9e6abf9bb8f2da03a8dfcbc322f921f1d
dortiz02 <dortiz.romero@udea.edu.co>
2026-09-20 17:11:40 -0500

    Create vista.h

---
+++ include/vista.h
@@ -0,0 +1,10 @@
+#ifndef VISTA_H
+#define VISTA_H
+
+// Imprime el tablero mostrando cada byte en su representacin binaria.
+void mostrarTableroBinario(unsigned char* tablero, int bytesReservados);
+
+// Imprime el tablero traduciendo cada posicion a su carcter representativo.
+void mostrarTableroFichas(unsigned char* tablero, int filas, int columnas);
+
+#endif // VISTA_H
