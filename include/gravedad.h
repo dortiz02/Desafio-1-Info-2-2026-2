ffc743de95014cdb07c923111d09ba2bbaac3721
dortiz02 <dortiz.romero@udea.edu.co>
2026-09-20 18:41:33 -0500

    Create gravedad.h
    se declara aplicarGravedad, encargada solamente de reacomodar las fichas existentes dentro de cada columna. El relleno con fichas nuevas se deja para una funcion aparte, que se agrega en el proximo commit.

---
+++ include/gravedad.h
@@ -0,0 +1,8 @@
+#ifndef GRAVEDAD_H
+#define GRAVEDAD_H
+
+// Para cada columna, desplaza las fichas no vacias hacia abajo, dejando
+// las posiciones vacias agrupadas en la parte superior de la columna.
+void aplicarGravedad(unsigned char* tablero, int filas, int columnas);
+
+#endif // GRAVEDAD_H
