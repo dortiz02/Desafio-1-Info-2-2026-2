54bfe3a324dce59759aaa80fb5193d453630c2a9
dortiz02 <dortiz.romero@udea.edu.co>
2026-09-20 17:08:44 -0500

    Update tablero.cpp
    se corrige el bug detectado al probar posiciones donde el desplazamiento es 6 o 7: en esos casos los 3 bits de la ficha quedan repartidos entre el byte actual y el siguiente, y la máscara de un solo byte perdía el bit que caía en el byte contiguo. Ahora obtenerFicha y colocarFicha combinan (y separan) los dos bytes involucrados solo cuando es necesario, evitando además acceder fuera del bloque reservado cuando el byte actual es el último del tablero.

--- src/tablero.cpp
+++ src/tablero.cpp
@@ -28,10 +28,19 @@ unsigned char obtenerFicha(unsigned char* tablero, int fila, int columna, int co
     int byteIndice = bitInicial / 8;
     int bitOffset = bitInicial % 8;
 
-    unsigned char mascara = 0b111 << bitOffset;
-    unsigned char valor = (tablero[byteIndice] & mascara) >> bitOffset;
+    // Se combina el byte actual con el siguiente en un valor de 16 bits,
+    // pero solo cuando la ficha realmente cruza hacia el siguiente byte
+    // (esto evita leer memoria fuera del bloque reservado cuando el
+    // byte actual es el ultimo del tablero).
+    unsigned int dosBytes = tablero[byteIndice];
+    if (bitOffset > 5) {
+        dosBytes = dosBytes | ((unsigned int)tablero[byteIndice + 1] << 8);
+    }
 
-    return valor;
+    unsigned int mascara = (unsigned int)0b111 << bitOffset;
+    unsigned int valor = (dosBytes & mascara) >> bitOffset;
+
+    return (unsigned char)valor;
 }
 
 void colocarFicha(unsigned char* tablero, int fila, int columna, int columnas, unsigned char valorFicha)
@@ -42,7 +51,15 @@ void colocarFicha(unsigned char* tablero, int fila, int columna, int columnas, u
     int byteIndice = bitInicial / 8;
     int bitOffset = bitInicial % 8;
 
-    unsigned char mascaraLimpiar = ~(0b111 << bitOffset);
-    tablero[byteIndice] = tablero[byteIndice] & mascaraLimpiar;
-    tablero[byteIndice] = tablero[byteIndice] | (valorFicha << bitOffset);
+    unsigned int mascaraFicha = (unsigned int)0b111 << bitOffset;
+    unsigned int mascaraLimpiar = ~mascaraFicha;
+    unsigned int valorDesplazado = (unsigned int)valorFicha << bitOffset;
+
+    tablero[byteIndice] = (unsigned char)(tablero[byteIndice] & (mascaraLimpiar & 0xFF));
+    tablero[byteIndice] = (unsigned char)(tablero[byteIndice] | (valorDesplazado & 0xFF));
+
+    if (bitOffset > 5) {
+        tablero[byteIndice + 1] = (unsigned char)(tablero[byteIndice + 1] & ((mascaraLimpiar >> 8) & 0xFF));
+        tablero[byteIndice + 1] = (unsigned char)(tablero[byteIndice + 1] | ((valorDesplazado >> 8) & 0xFF));
+    }
 }
