204232ffa5df39d4bbd1c0ddef7f430ee081044f
dortiz02 <dortiz.romero@udea.edu.co>
2026-09-20 18:55:57 -0500

    Update gravedad.h
    se agrega la declaracion de generarNuevasFichas, que completa el ciclo de reorganizacion junto con aplicarGravedad.

--- include/gravedad.h
+++ include/gravedad.h
@@ -5,4 +5,9 @@
 // las posiciones vacias agrupadas en la parte superior de la columna.
 void aplicarGravedad(unsigned char* tablero, int filas, int columnas);
 
+// Recorre todo el tablero y coloca una ficha aleatoria en cada posicion
+// que este en estado VACIO. Se usa despues de aplicarGravedad, para
+// recuperar las dimensiones completas del tablero.
+void generarNuevasFichas(unsigned char* tablero, int filas, int columnas);
+
 #endif // GRAVEDAD_H
