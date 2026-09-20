26901e471e38f1e6e6d849fe319fb49165e55b0f
dortiz02 <dortiz.romero@udea.edu.co>
2026-09-20 16:32:09 -0500

    Update tablero.h

--- include/tablero.h
+++ include/tablero.h
@@ -3,11 +3,15 @@
 
 // Crea un tablero de filas x columnas, reservando la cantidad mínima
 // de bytes necesaria para almacenar 3 bits por posición.
-// Devuelve el puntero al bloque de memoria reservado y, por referencia,
-// la cantidad de bytes reservados (capacidad física inicial).
 unsigned char* crearTablero(int filas, int columnas, int &bytesReservados);
 
 // Libera la memoria asociada a un tablero.
 void liberarTablero(unsigned char* tablero);
+
+// Obtiene el codigo de 3 bits almacenado en (fila, columna).
+unsigned char obtenerFicha(unsigned char* tablero, int fila, int columna, int columnas);
+
+// Escribe el codigo de 3 bits valorFicha en (fila, columna).
+void colocarFicha(unsigned char* tablero, int fila, int columna, int columnas, unsigned char valorFicha);
 
 #endif // TABLERO_H
