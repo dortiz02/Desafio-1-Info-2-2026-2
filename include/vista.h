715e8049a49232a6190d13058a288085aa20b3b0
dortiz02 <dortiz.romero@udea.edu.co>
2026-09-20 21:43:48 -0500

    Update vista.h

--- include/vista.h
+++ include/vista.h
@@ -1,10 +1,11 @@
 #ifndef VISTA_H
 #define VISTA_H
 
-// Imprime el tablero mostrando cada byte en su representacin binaria.
 void mostrarTableroBinario(unsigned char* tablero, int bytesReservados);
-
-// Imprime el tablero traduciendo cada posicion a su carcter representativo.
 void mostrarTableroFichas(unsigned char* tablero, int filas, int columnas);
+
+// Imprime dimensiones, eliminaciones, fichas eliminadas, combos,
+// cascadas y puntuacion. Se llama tras cada jugada.
+void mostrarEstadoJuego(int filas, int columnas);
 
 #endif // VISTA_H
